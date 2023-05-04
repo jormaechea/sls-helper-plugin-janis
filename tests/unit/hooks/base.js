5dc8a6c98971051d711ce4f3688f70504c150ed1
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2023-05-04 13:30:22 -0300

    Added VPC access managed policy always

--- tests/unit/hooks/base.js
+++ tests/unit/hooks/base.js
@@ -295,6 +295,7 @@ describe('Hooks', () => {
 										}
 									]
 								},
+								ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole'],
 								Policies: [
 									{
 										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
@@ -721,6 +722,7 @@ describe('Hooks', () => {
 										}
 									]
 								},
+								ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole'],
 								Policies: [
 									{
 										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
@@ -909,417 +911,6 @@ describe('Hooks', () => {
 					stage: '${opt:stage, \'local\'}',
 					region: '${opt:region, \'us-east-1\'}',
 					role: 'ServiceExecutionRole',
-					endpointType: 'REGIONAL',
-					apiName: 'JANIS ${param:humanReadableStage} ${self:custom.serviceTitle} API',
-					logRetentionInDays: 14,
-					environment: {
-						JANIS_SERVICE_NAME: '${self:custom.serviceCode}',
-						JANIS_ENV: '${self:custom.stage}',
-						MS_PATH: 'src'
-					},
-					tags: {
-						Owner: 'Janis',
-						Microservice: '${self:custom.serviceName}',
-						Stack: '${param:humanReadableStage}'
-					},
-					versionFunctions: false,
-					apiGateway: {
-						disableDefaultEndpoint: true,
-						minimumCompressionSize: 1024
-					},
-					logs: {
-						restApi: {
-							accessLogging: true,
-							executionLogging: false,
-							level: 'INFO',
-							fullExecutionData: false,
-							format: JSON.stringify({
-								date: '$context.requestTime',
-								reqId: '$context.requestId',
-								integReqId: '$context.integration.requestId',
-								ip: '$context.identity.sourceIp',
-								ua: '$context.identity.userAgent',
-								clientCode: '$context.authorizer.clientCode',
-								principalId: '$context.authorizer.principalId',
-								reqMethod: '$context.httpMethod',
-								path: '$context.resourcePath',
-								realPath: '$context.path',
-								status: '$context.status',
-								authTime: '$context.authorizer.latency',
-								resTime: '$context.responseLatency',
-								gwError: '$context.error.message',
-								integError: '$context.integration.error'
-							})
-						}
-					}
-				},
-				params: {
-					local: {
-						humanReadableStage: 'Local',
-						janisDomain: 'janis.localhost'
-					},
-					beta: {
-						humanReadableStage: 'Beta',
-						janisDomain: 'janisdev.in'
-					},
-					qa: {
-						janisDomain: 'janisqa.in',
-						humanReadableStage: 'QA'
-					},
-					prod: {
-						humanReadableStage: 'Prod',
-						janisDomain: 'janis.in'
-					}
-				},
-				package: {
-					individually: false,
-					include: [
-						'src/config/*'
-					],
-					exclude: [
-						'.nyc_output/**',
-						'.bitbucket/**',
-						'.deploy/**',
-						'.husky/**',
-						'view-schemas/**',
-						'view-schemas-built/**',
-						'view-schemas-built-local/**',
-						'tests/**',
-						'test-reports/**',
-						'hooks/**',
-						'events/**',
-						'permissions/**',
-						'schemas/src/**',
-						'serverless/**',
-						'src/environments/**',
-						'*',
-						'.*',
-						'node_modules/.cache/**',
-						'node_modules/**/README.md',
-						'node_modules/**/.github/**',
-						'node_modules/**/CHANGELOG.md',
-						'node_modules/**/LICENSE',
-						'node_modules/**/*.js.map',
-						'node_modules/**/*.map',
-						'node_modules/**/*.min.map',
-						'node_modules/**/*.js.flow',
-						'node_modules/**/*.d.ts',
-						'node_modules/function.prototype.name/**',
-						'node_modules/which-typed-array/**',
-						'node_modules/is-typed-array/**',
-						'mongodb/src/**',
-						'bson/dist/**',
-						'bson/src/**',
-						'node_modules/@aws-sdk/**',
-						'node_modules/**/@aws-sdk/**',
-						'node_modules/sinon/**',
-						'node_modules/serverless/**',
-						'node_modules/@serverless/**',
-						'node_modules/@babel/**',
-						'node_modules/eslint-plugin-import/**',
-						'node_modules/@sinonjs/**',
-						'node_modules/faker/dist/**',
-						'node_modules/date-fns/esm/**',
-						'node_modules/date-fns/fp/**',
-						'node_modules/**/date-fns/docs/**',
-						'node_modules/**/buffer/test/**',
-						'node_modules/**/jmespath/test/**',
-						'node_modules/**/qs/test/**',
-						'node_modules/**/qs/dist/**',
-						'node_modules/**/bson/browser_build/**',
-						'node_modules/**/axios/dist/browser/**',
-						'node_modules/**/axios/dist/esm/**'
-					]
-				},
-				custom: {
-					serviceTitle: 'Testing',
-					serviceName: 'Testing',
-					serviceCode: 'testing',
-					stage: '${self:provider.stage}',
-					region: '${self:provider.region}',
-
-					humanReadableStage: {
-						local: 'Local',
-						beta: 'Beta',
-						qa: 'QA',
-						prod: 'Prod'
-					},
-
-					janisDomains: {
-						local: 'janis.localhost',
-						beta: 'janisdev.in',
-						qa: 'janisqa.in',
-						prod: 'janis.in'
-					},
-
-					cacheEnabled: {
-						prod: false
-					},
-
-					customDomain: {
-						domainName: '${self:custom.serviceCode}.${param:janisDomain}',
-						basePath: 'api',
-						stage: '${self:custom.stage}',
-						createRoute53Record: true,
-						endpointType: 'regional',
-						securityPolicy: 'tls_1_2'
-					},
-
-					apiGatewayCaching: {
-						enabled: '${self:custom.cacheEnabled.${self:custom.stage}, \'false\'}',
-						clusterSize: '0.5',
-						ttlInSeconds: 600 // 10 minutos
-					},
-
-					'serverless-offline': {
-						httpPort: 3000,
-						lambdaPort: 23000,
-						host: '0.0.0.0',
-						stage: 'local',
-						noPrependStageInUrl: true,
-						prefix: 'api',
-						reloadHandler: true
-					},
-
-					stageVariables: {
-						serviceName: '${self:custom.serviceCode}'
-					},
-
-					reducer: {
-						ignoreMissing: true
-					}
-				},
-				plugins: [
-					'serverless-domain-manager',
-					'serverless-offline',
-					'serverless-api-gateway-caching',
-					'serverless-plugin-stage-variables',
-					'@janiscommerce/serverless-plugin-remove-authorizer-permissions',
-					'serverless-plugin-split-stacks'
-				],
-				resources: {
-					Resources: {
-
-						ServiceExecutionRole: {
-							Type: 'AWS::IAM::Role',
-							Properties: {
-								RoleName: 'Janis${self:custom.serviceName}Service-${self:custom.stage}-lambdaRole',
-								Path: '/janis-service/',
-								AssumeRolePolicyDocument: {
-									Version: '2012-10-17',
-									Statement: [
-										{
-											Effect: 'Allow',
-											Principal: {
-												Service: [
-													'lambda.amazonaws.com'
-												]
-											},
-											Action: 'sts:AssumeRole'
-										}
-									]
-								},
-								Policies: [
-									{
-										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
-										PolicyDocument: {
-											Version: '2012-10-17',
-											Statement: [
-												{
-													Effect: 'Allow',
-													Action: [
-														'logs:CreateLogGroup',
-														'logs:CreateLogStream',
-														'logs:PutLogEvents'
-													],
-													Resource: [
-														{
-															'Fn::Join': [
-																':',
-																[
-																	'arn:aws:logs',
-																	{ Ref: 'AWS::Region' },
-																	{ Ref: 'AWS::AccountId' },
-																	'log-group:/aws/lambda/*:*'
-																]
-															]
-														},
-														{
-															'Fn::Join': [
-																':',
-																[
-																	'arn:aws:logs',
-																	{ Ref: 'AWS::Region' },
-																	{ Ref: 'AWS::AccountId' },
-																	'log-group:/aws/lambda/*:*:*'
-																]
-															]
-														}
-													]
-												}
-											]
-										}
-									}
-								]
-							}
-						},
-
-						UnauthorizedResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'UNAUTHORIZED',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '401'
-							}
-						},
-
-						BadRequestBodyResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"detail":"$context.authorizer.errorMessage","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'BAD_REQUEST_BODY',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '400'
-							}
-						},
-
-						BadRequestParameters: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"detail":"$context.authorizer.errorMessage","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'BAD_REQUEST_PARAMETERS',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '400'
-							}
-						},
-
-						AccessDeniedResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"detail":"$context.authorizer.errorMessage","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'ACCESS_DENIED',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '403'
-							}
-						},
-
-						AuthorizerConfigurationErrorResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"detail":"$context.authorizer.errorMessage","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'AUTHORIZER_CONFIGURATION_ERROR',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '500'
-							}
-						},
-
-						AuthorizerFailureResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":$context.error.messageString,"detail":"$context.authorizer.errorMessage","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'AUTHORIZER_FAILURE',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '500'
-							}
-						},
-
-						IntegrationTimeoutResponse: {
-							Type: 'AWS::ApiGateway::GatewayResponse',
-							Properties: {
-								ResponseParameters: {
-									'gatewayresponse.header.Access-Control-Allow-Origin': 'method.request.header.Origin'
-								},
-								ResponseTemplates: {
-									'application/json': '{"message":"Timeout","authorizerErrorType":"$context.error.responseType"}'
-								},
-								ResponseType: 'INTEGRATION_TIMEOUT',
-								RestApiId: {
-									Ref: 'ApiGatewayRestApi'
-								},
-								StatusCode: '504'
-							}
-						}
-					}
-				}
-			});
-		});
-
-		it('Should add the VPC Access managed policy if vpc is configured', () => {
-
-			const serviceConfig = base({
-				provider: {
-					vpc: {
-						securityGroupId: 'sg-123456',
-						subnetIds: [
-							'sub-123456',
-							'sub-123457'
-						]
-					}
-				}
-			}, {
-				serviceCode: 'testing',
-				servicePort: validServicePort
-			});
-
-			assert.deepStrictEqual(serviceConfig, {
-				service: 'Janis${self:custom.serviceName}Service',
-				provider: {
-					name: 'aws',
-					runtime: 'nodejs18.x',
-					memorySize: 1024,
-					stage: '${opt:stage, \'local\'}',
-					region: '${opt:region, \'us-east-1\'}',
-					role: 'ServiceExecutionRole',
-					vpc: {
-						securityGroupId: 'sg-123456',
-						subnetIds: [
-							'sub-123456',
-							'sub-123457'
-						]
-					},
 					endpointType: 'REGIONAL',
 					apiName: 'JANIS ${param:humanReadableStage} ${self:custom.serviceTitle} API',
 					logRetentionInDays: 14,
@@ -1896,6 +1487,7 @@ describe('Hooks', () => {
 										}
 									]
 								},
+								ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole'],
 								Policies: [
 									{
 										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
@@ -2301,6 +1893,7 @@ describe('Hooks', () => {
 										}
 									]
 								},
+								ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole'],
 								Policies: [
 									{
 										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
@@ -2711,6 +2304,7 @@ describe('Hooks', () => {
 										}
 									]
 								},
+								ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole'],
 								Policies: [
 									{
 										PolicyName: 'janis-${self:custom.serviceCode}-logs-policy',
