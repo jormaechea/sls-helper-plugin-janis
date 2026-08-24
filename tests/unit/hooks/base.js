3f2a8593ddd997e82f727adbe9b83eb2f93778cd
Manuel Vilche <manuel.vilche@janis.im>
2026-08-24 13:31:20 -0300

    Revert "add ServerlessApiGatewayCloudWatchRole resource"
    This reverts commit 515f464b02339c59806343c8c8ff5d6c047488e8.

--- tests/unit/hooks/base.js
+++ tests/unit/hooks/base.js
@@ -75,7 +75,6 @@ describe('Hooks', () => {
 				},
 				logs: {
 					restApi: {
-						role: { 'Fn::GetAtt': ['serverlessApiGatewayCloudWatchRole', 'Arn'] },
 						accessLogging: true,
 						executionLogging: false,
 						level: 'INFO',
@@ -424,23 +423,6 @@ describe('Hooks', () => {
 								Ref: 'ApiGatewayRestApi'
 							},
 							StatusCode: '504'
-						}
-					},
-
-					ServerlessApiGatewayCloudWatchRole: {
-						Type: 'AWS::IAM::Role',
-						Properties: {
-							AssumeRolePolicyDocument: {
-								Version: '2012-10-17',
-								Statement: [{
-									Effect: 'Allow',
-									Principal: {
-										Service: ['apigateway.amazonaws.com']
-									},
-									Action: 'sts:AssumeRole'
-								}]
-							},
-							ManagedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs']
 						}
 					}
 
